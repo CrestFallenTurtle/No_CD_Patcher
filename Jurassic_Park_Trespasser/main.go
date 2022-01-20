de375f1f836f8465f397aadcd58b5ba57338f538
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2022-01-20 11:05:06 +0100

    Worked on some major reworks

--- Jurassic_Park_Trespasser/main.go
+++ Jurassic_Park_Trespasser/main.go
@@ -10,21 +10,30 @@ import (
 
 func main() {
 	arg.Argument_add("--help", "-h", false, "Shows all available arguments and their purpose", []string{"NULL"})
-	arg.Argument_add("--exe", "-x", true, "Path to Trespasser exe to patch [REQUIRED]", []string{"NULL"})
-	arg.Argument_add("--reverse", "-r", true, "Path to Trespasser exe to reverse any changes made", []string{"NULL"})
+	arg.Argument_add("--exe", "-x", true, "Path to Trespasser exe [REQUIRED]", []string{"NULL"})
+	arg.Argument_add("--smks", "-s", true, "Path to a folder containing the four smk's [REQUIRED]", []string{"NULL"})
 	arg.Argument_parse()
 
 	if len(os.Args) > 1 {
 		if arg.Argument_check("-h") { // Print help screen
 			arg.Argument_help()
-		} else if arg.Argument_check("-r") { // Reverse changes
-			patcher.Begin_rollback(arg.Argument_get("-r"))
 		} else {
+			var path_to_exe string
+			var path_to_smks string
+
 			if arg.Argument_check("-x") { // Patch exe
-				patcher.Begin_patch(arg.Argument_get("-x"))
+				path_to_exe = arg.Argument_get("-x")
 			} else {
 				notify.Error("No exe file was provided", "main.main()")
 			}
+
+			if arg.Argument_check("-s") { // Patch exe
+				path_to_exe = arg.Argument_get("-s")
+			} else {
+				notify.Error("No folder was provided", "main.main()")
+			}
+
+			patcher.Begin_patch(path_to_exe, path_to_smks)
 		}
 	} else {
 		notify.Error("No argument was provided, run '--help'/'-h' to have a look at the arguments available", "main.main()")
--- Jurassic_Park_Trespasser/utility/patcher/patcher.go
+++ Jurassic_Park_Trespasser/utility/patcher/patcher.go
@@ -1,135 +1,216 @@
 package patcher
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
 	"io"
 	"os"
+	"runtime"
 	"strings"
 
 	"github.com/s9rA16Bf4/notify_handler/go/notify"
+	"golang.org/x/sys/windows/registry"
 )
 
 type file_t struct {
-	gut  []string // Contains everything in the target file
-	name string   // File name
-	path string   // File path
+	name               string // File name
+	path_to_exe        string // File path
+	path_to_folder     string // Folder path
+	path_to_credits    string
+	path_to_newgame    string
+	path_to_tpassintro string
+	path_to_win        string
 }
 
 var c_file file_t
 
 const (
-	EXE_NAME   = "trespass.exe" // Target exe name
-	EXE_BACKUP = EXE_NAME + "_backup"
+	EXE_NAME        = "trespass.exe" // Target exe name
+	EXE_HASH        = "4da71e51e08a7ebea1ecd2caf72482c54a8c07f09af1d44330731f3c554e4f5c6a40eab90856da90518a5735be4783f8344ec263eaf7072aa98ee7c05fa3b08f"
+	CREDITS_HASH    = "e775e63a2874edf2f463d46c22216e48d498cb3c2e8d1af212b0d759e653d350e139cb827c97692de142dd179a6cedeba15f420f4b09cfdc918d354da77565db"
+	NEWGAME_HASH    = "3d5ec239a026ae10713ebf600ad1ef9e1a647f041d7ea004a3e7a8ed977a09d56b10613de58bf8c2b839ea6dec2e0730acb103071e999ca00492d0b34c9eef2f"
+	TPASSINTRO_HASH = "fef933abe8af3ce32a0c859bb7e75c782a8790df89635a796039f6b6cc94b9a70170edf86637babf89dcfc5c218319b239c05b668c64d46806bcd957c8b3443d"
+	WIN_HASH        = "2f0fb246948b1cbeec818459ba3fbc4ea942a2450a09f23f3bd6ea10ee4f1aa0d764e2afa561343cc1f7c7031e0ebe61e5bdbfe984d4b5f9992d0f6f5ba11499"
 )
 
-func Begin_rollback(file_path string) {
+func Begin_patch(file_path_exe string, file_path_smks string) {
+	if runtime.GOOS != "windows" {
+		notify.Error("This tool works only on windows", "patcher.Begin_patch")
+	}
 	c_file.name = EXE_NAME
-	c_file.path = file_path
-	read_file()
+	c_file.path_to_exe = file_path_exe
+	c_file.path_to_folder = file_path_smks
 
-	notify.Inform("Phase 1: Creating a backup of the target")
-	create_backup()
+	notify.Inform("[Phase 1] Checking if target exe is correct")
+	check_exe()
 
-	notify.Inform("Phase 2: Removes the no-cd patch")
-	patch_out_cd(true)
+	notify.Inform("[Phase 2] Checking if the provided smks are correct")
+	check_smk()
 
-	notify.Inform("Phase 3: Removes the patch for the error that occurs")
-	patch_out_cd_error(true)
+	notify.Inform("[Phase 3] Creating the directories")
+	create_dir()
 
-	notify.Inform("Phase 4: Saving edits to the binary file")
-	save_exe()
+	notify.Inform("[Phase 4] Copying 'credits.smk'")
+	copy_credits()
 
-	notify.Inform("Phase 5: Done. The result has been written to '" + EXE_NAME + "' and a backup, '" + EXE_BACKUP + "', has been created in the local directory if you would like to roll back the changes")
+	notify.Inform("[Phase 5] Copying 'newgame.smk'")
+	copy_newgame()
+
+	notify.Inform("[Phase 6] Copying 'tpassintro.smk'")
+	copy_tpassintro()
+
+	notify.Inform("[Phase 7] Copying 'win.smk'")
+	copy_win()
+
+	notify.Inform("[Phase 8] Changing key value in regedit")
+	regedit()
+
+	notify.Inform("All done over here, enjoy over a hot cup of coffee or tea")
 }
 
-func Begin_patch(file_path string) {
-	c_file.name = EXE_NAME
-	c_file.path = file_path
-
-	read_file()
-
-	notify.Inform("Phase 1: Creating a backup of the target")
-	create_backup()
-
-	notify.Inform("Phase 2: Patching the requirement to have a cd inserted.")
-	patch_out_cd(false)
-
-	notify.Inform("Phase 3: Patching an error that would occur if the cd is not present.")
-	patch_out_cd_error(false)
-
-	notify.Inform("Phase 4: Saving edits to the binary file")
-	save_exe()
-
-	notify.Inform("Phase 5: Done. The result has been written to '" + EXE_NAME + "' and a backup, '" + EXE_BACKUP + "', has been created in the local directory if you would like to roll back the changes")
-}
-
-func read_file() {
-	if strings.Contains(c_file.path, EXE_NAME) { // This is what the exe is called
-		out, err := os.Open(c_file.path)
-		if err != nil {
-			notify.Error(err.Error(), "patcher.read_file()")
-		}
-		c_file.gut = make([]string, 0)
-		local := make([]byte, 12)
-		for {
-			_, err = out.Read(local)
-			if err == io.EOF {
-				break
+func internal_hash(path_to_file string) string {
+	exe, err := os.Open(path_to_file)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.internal_hash")
+	}
+	exe_size, err := exe.Stat()
+	if err != nil {
+		notify.Error(err.Error(), "patcher.internal_hash")
+	}
+	buf := make([]byte, exe_size.Size())
+	hash := sha256.New()
+	for {
+		roof, err := exe.Read(buf)
+		if err == io.EOF || err != nil {
+			break
+		} else {
+			_, err := hash.Write(buf[:roof])
+			if err != nil {
+				notify.Error(err.Error(), "patcher.internal_hash")
 			}
-			temp := hex.EncodeToString(local) // This makes it a hella lot easier to handle
-			c_file.gut = append(c_file.gut, temp)
 		}
-		out.Close()
+	}
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func check_exe() {
+	if strings.Contains(c_file.path_to_exe, EXE_NAME) { // This is what the exe is called
+		hash_sum := internal_hash(c_file.path_to_exe)
+		if hash_sum != EXE_HASH {
+			notify.Error("Hash values for the exe didn't match. Assuming it's not the correct exe.", "patcher.check_exe")
+		}
 	} else {
-		notify.Error("The provided file '"+c_file.path+"' doesn't seem to the target exe '"+EXE_NAME+"'", "patcher.read_file()")
+		notify.Error("The provided file '"+c_file.path_to_exe+"' doesn't seem to the target exe '"+EXE_NAME+"'", "patcher.read_file()")
 	}
 }
 
-func create_backup() {
-	dst, err := os.Create(EXE_BACKUP) // Will create it in the local directory
+func check_smk() {
+	folder, err := os.Open(c_file.path_to_folder)
 	if err != nil {
-		notify.Error(err.Error(), "patcher.create_backup()")
+		notify.Error(err.Error(), "patcher.check_smk()")
 	}
-	src, err := os.Open(c_file.path) // Open the file
+	files, err := folder.ReadDir(0) // Reads all the contents
 	if err != nil {
-		notify.Error(err.Error(), "patcher.create_backup()")
+		notify.Error(err.Error(), "patcher.check_smk()")
 	}
-	io.Copy(dst, src) // Copy everything from the src to the dst
+	hash_smk := make(map[string]string)
+	hash_smk["credits.smk"] = "NULL"
+	hash_smk["newgame.smk"] = "NULL"
+	hash_smk["tpassintro.smk"] = "NULL"
+	hash_smk["win.smk"] = "NULL"
+
+	for _, file := range files {
+		switch file.Name() {
+		case "credits.smk":
+			c_file.path_to_credits = c_file.path_to_folder + "credits.smk"
+			hash_smk["credits.smk"] = internal_hash(file.Name())
+		case "newgame.smk":
+			c_file.path_to_newgame = c_file.path_to_folder + "newgame.smk"
+			hash_smk["newgame.smk"] = internal_hash(file.Name())
+		case "tpassintro.smk":
+			c_file.path_to_tpassintro = c_file.path_to_folder + "tpassintro.smk"
+			hash_smk["tpassintro.smk"] = internal_hash(file.Name())
+		case "win.smk":
+			c_file.path_to_win = c_file.path_to_folder + "win.smk"
+			hash_smk["win.smk"] = internal_hash(file.Name())
+		}
+	}
+	if hash_smk["credits.smk"] != CREDITS_HASH ||
+		hash_smk["newgame.smk"] != NEWGAME_HASH ||
+		hash_smk["tpassintro.smk"] != TPASSINTRO_HASH ||
+		hash_smk["win.smk"] != WIN_HASH {
+		notify.Error("One or more hashes for the smks was wrong", "patcher.check_smk()")
+	}
+}
+
+func create_dir() {
+	err := os.MkdirAll("C:\\Program Files (x86)\\DreamWorks Interactive\\Trespasser\\data\\menu", 0700)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.create_dir()")
+	}
+}
+
+func copy_credits() {
+	dst, err := os.Create("C:\\Program Files (x86)\\DreamWorks Interactive\\Trespasser\\data\\menu\\credits.smk")
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_credits()")
+	}
+	src, err := os.Open(c_file.path_to_credits)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_credits()")
+	}
+	io.Copy(dst, src)
+	dst.Close()
 	src.Close()
+}
+func copy_newgame() {
+	dst, err := os.Create("C:\\Program Files (x86)\\DreamWorks Interactive\\Trespasser\\data\\menu\\newgame.smk")
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_newgame()")
+	}
+	src, err := os.Open(c_file.path_to_newgame)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_newgame()")
+	}
+	io.Copy(dst, src)
 	dst.Close()
+	src.Close()
 }
-
-func patch_out_cd(reverse_work bool) {
-	if !reverse_work {
-		for i, line := range c_file.gut {
-			if line == "ff0f85a60000008b1db05064" {
-				c_file.gut[i] = "ff0f84a60000008b1db05064" // Equals jump if equal
-				break
-			}
-		}
-	} else {
-		for i, line := range c_file.gut {
-			if line == "ff0f84a60000008b1db05064" {
-				c_file.gut[i] = "ff0f85a60000008b1db05064"
-				break
-			}
-		}
+func copy_tpassintro() {
+	dst, err := os.Create("C:\\Program Files (x86)\\DreamWorks Interactive\\Trespasser\\data\\menu\\tpassintro.smk")
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_tpassintro()")
 	}
-}
-
-func patch_out_cd_error(reverse_work bool) {
-	if !reverse_work {
-
-	} else {
-
+	src, err := os.Open(c_file.path_to_tpassintro)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_tpassintro()")
 	}
-}
-
-func save_exe() {
-	dst, _ := os.Create(c_file.name) // Creates our modified exe
-	for _, line := range c_file.gut {
-		arr, _ := hex.DecodeString(line) // Write all the content
-		dst.Write(arr)
-	}
+	io.Copy(dst, src)
 	dst.Close()
+	src.Close()
+}
+func copy_win() {
+	dst, err := os.Create("C:\\Program Files (x86)\\DreamWorks Interactive\\Trespasser\\data\\menu\\win.smk")
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_win()")
+	}
+	src, err := os.Open(c_file.path_to_win)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.copy_win()")
+	}
+	io.Copy(dst, src)
+	dst.Close()
+	src.Close()
+}
+
+func regedit() {
+	reg, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\WOW6432Node\DreamWorks Interactive\Trespasser`, registry.WRITE)
+	if err != nil {
+		notify.Error(err.Error(), "patcher.regedit")
+	}
+	err = reg.SetStringValue("Data Drive", "C:\\Program Files (x86)\\DreamWorks Interactive\\Trespasser\\")
+	if err != nil {
+		notify.Error(err.Error(), "patcher.regedit")
+	}
+	reg.Close()
 }
